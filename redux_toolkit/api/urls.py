cbdc6f6631e043ba69a704b69cbbac43665a6546
motty93 <rdwbocungelt5@gmail.com>
2024-10-10 11:08:37 +0900

    不要なので削除

--- redux_toolkit/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls import include
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-
-urlpatterns = [
-    path('', include(router.urls))
-]
